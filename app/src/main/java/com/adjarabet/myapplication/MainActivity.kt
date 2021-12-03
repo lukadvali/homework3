dde3e142bd68d001e2e07db6e1433f4b35ab303a
lukadvali <luka.dvali.1@btu.edu.ge>
2021-12-03 20:44:52 +0400

    homework3

--- app/src/main/java/com/adjarabet/myapplication/MainActivity.kt
+++ app/src/main/java/com/adjarabet/myapplication/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.adjarabet.myapplication
 
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
     private lateinit var button1 : Button
     private lateinit var button2 : Button
     private lateinit var button3 : Button
@@ -22,6 +23,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     var firstPlayer = ArrayList<Int>()
     var secondPlayer = ArrayList<Int>()
     var activePlayer = 1
+    var counter = 0
+    lateinit var reset : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +33,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun init(){
-        ///adddedededsfjdlghsdghkjfdhgksjdfghkjfsdhgkjfdhkjghsdjkfghkjsfhgkjshfgjkshfgjhfdkj
+
         button1 = findViewById(R.id.button1)
         button2 = findViewById(R.id.button2)
         button3 = findViewById(R.id.button3)
@@ -40,6 +43,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button7 = findViewById(R.id.button7)
         button8 = findViewById(R.id.button8)
         button9 = findViewById(R.id.button9)
+        reset = findViewById(R.id.reset)
 
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
@@ -51,7 +55,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button8.setOnClickListener(this)
         button9.setOnClickListener(this)
 
+        reset.setOnClickListener{
+            resetBoard();
+        }
     }
+
+    private fun resetBoard(){
+        firstPlayer.clear();
+        secondPlayer.clear();
+        button1.text = "";
+        button2.text = "";
+        button3.text = "";
+        button4.text = "";
+        button5.text = "";
+        button6.text = "";
+        button7.text = "";
+        button8.text = "";
+        button9.text = "";
+        button1.isEnabled = true;
+        button2.isEnabled = true;
+        button3.isEnabled = true;
+        button4.isEnabled = true;
+        button5.isEnabled = true;
+        button6.isEnabled = true;
+        button7.isEnabled = true;
+        button8.isEnabled = true;
+        button9.isEnabled = true;
+        button1.setBackgroundColor(Color.rgb(98, 0, 238));
+        button2.setBackgroundColor(Color.rgb(98, 0, 238));
+        button3.setBackgroundColor(Color.rgb(98, 0, 238));
+        button4.setBackgroundColor(Color.rgb(98, 0, 238));
+        button5.setBackgroundColor(Color.rgb(98, 0, 238));
+        button6.setBackgroundColor(Color.rgb(98, 0, 238));
+        button7.setBackgroundColor(Color.rgb(98, 0, 238));
+        button8.setBackgroundColor(Color.rgb(98, 0, 238));
+        button9.setBackgroundColor(Color.rgb(98, 0, 238));
+        counter = 0;
+    }
+
 
     private fun playGame(clickedView: Button, buttonNumber: Int){
         if (activePlayer == 1){
@@ -59,16 +100,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             clickedView.setBackgroundColor(Color.RED)
             activePlayer = 2
             firstPlayer.add(buttonNumber)
-
+            counter++;
         } else {
             clickedView.text = "0"
             clickedView.setBackgroundColor(Color.BLUE)
             activePlayer = 1
             secondPlayer.add(buttonNumber)
+            counter++;
         }
         clickedView.isEnabled = false
         check()
     }
+
 
     private fun check(){
         var winner = 0
@@ -125,6 +168,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             Toast.makeText(this, "first player won", Toast.LENGTH_SHORT).show()
         } else if (winner == 2){
             Toast.makeText(this, "second player won", Toast.LENGTH_SHORT).show()
+        }else if(counter == 9){
+            Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()
         }
 
     }
